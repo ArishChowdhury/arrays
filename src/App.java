ebcaa0f6fb6a27b82971798f86ac4386eeb26d61
Arish <arishchowdhury.2021@gmail.com>
2024-12-24 22:39:07 +0600

    i made this

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args){
 
-        //Create an integar array and initialize
+        
         int array[] = new int[3];
         array[0] = 0;
         array[1] = 1;
@@ -14,7 +14,7 @@ public class App {
 
         System.out.println("---------------------------------------------------------------------------------");
 
-        //Create the integar array and initialize it in a different way
+       
 
         int[] array2 = {1,2,3};
         for (int i = 0; i < array2.length; i++) {
